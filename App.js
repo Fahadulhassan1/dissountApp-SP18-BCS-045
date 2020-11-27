4a7a5791f12bee9cfc26fb6aa7101e4dcad84095
unknown <you@example.com>
2020-11-27 21:26:50 +0500

    commit

--- App.js
+++ App.js
@@ -14,12 +14,20 @@ import {
 
 
 export default function App() {
-  const [getPrice , setPrice] = useState ("dummy text");
-  const [getDiscount , setDiscount] = useState ("dummydiscount");
+  const [getPrice , setPrice] = useState ("");
+  const [getDiscount , setDiscount] = useState ("");
+  const [gettotalDiscount , settotalDiscount ] = useState("");
+  const [getsavings , setsavings  ] = useState ("");
 
   const calculate =  () =>{
       const price = Number (getPrice)
       const discount = Number (getDiscount)
+      const totalDiscount = Number (price - (price * discount)/100)
+      const savings = Number (price - totalDiscount)
+      settotalDiscount(String(totalDiscount))
+      setsavings(String(savings))
+      
+
       
 
   }
@@ -30,9 +38,10 @@ export default function App() {
     <Text> enter the pdiscount</Text>
     <TextInput style = {styles.inputtext} onChangeText = {text => setDiscount(text)} /> 
     <Button title = " calculate"  onPress = {calculate}/>
+    
 
-    <Text>  {getPrice} </Text>
-     <Text>{getDiscount} </Text>
+    <Text> final price is : {gettotalDiscount} </Text>
+     <Text> total saving is : {getsavings} </Text>
 
 
     
