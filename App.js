9347f904e9fdc7b65092cb9ae29bf0a32791cf5a
unknown <you@example.com>
2020-11-27 22:36:31 +0500

    commmit

--- App.js
+++ App.js
@@ -22,27 +22,43 @@ export default function App() {
   const calculate =  () =>{
       const price = Number (getPrice)
       const discount = Number (getDiscount)
-      const totalDiscount = Number (price - (price * discount)/100)
-      const savings = Number (price - totalDiscount)
+      const totalDiscount = Number (price - (price * discount)/100).toFixed(2)
+      const savings = Number (price - totalDiscount).toFixed(2)
       settotalDiscount(String(totalDiscount))
       setsavings(String(savings))
-      
-
-      
-
   }
+    const discountCheck = (e) =>{
+      const check = Number (e)
+      if(check < 0 || check > 100){
+        alert("you entered wrong discount")
+        setDiscount("")
+      }else{
+        setDiscount(e)
+      }
+
+    } 
+
+      
+  
   return(
-    <View style = {{paddingTop : 40}}> 
-    <Text> enter the price</Text>
-    <TextInput style = {styles.inputtext} onChangeText = {text => setPrice(text)}  /> 
-    <Text> enter the pdiscount</Text>
-    <TextInput style = {styles.inputtext} onChangeText = {text => setDiscount(text)} /> 
-    <Button title = " calculate"  onPress = {calculate}/>
+    <View style = {{backgroundColor: 'orange'}}> 
+    <Text style ={styles.header}>Discount App</Text>
+    <View style = {styles.priceborder}>
+    <Text style = {{fontSize : 16}}> enter the price</Text>
+    <TextInput style = {styles.inputtext} onChangeText = {text => setPrice(text)} keyboardType={'numeric'}  /> 
+    </View>
+    < View style = {styles.priceborder} >
+    <Text style = {{fontSize : 16}}> enter the discount</Text>
+    <TextInput style = {styles.inputtext} keyboardType={'numeric'}  onChangeText = {(text) => discountCheck(text)}  /> 
+    </View>
+    <Button  color = "green" title = " calculate"  onPress = {calculate}/>
     
-
-    <Text> final price is : {gettotalDiscount} </Text>
-     <Text> total saving is : {getsavings} </Text>
-
+    <View style={{marginTop : 50  , borderBottomWidth:2}}>
+    <Text style = {{fontSize : 24}}> final price is : {gettotalDiscount} </Text>
+    </View>
+    <View  style={{marginTop : 50  , borderBottomWidth:2}}>
+     <Text style = {{fontSize : 24}}> total saving is : {getsavings} </Text>
+     </View>
 
     
     
@@ -53,7 +69,21 @@ export default function App() {
 }
 const styles = StyleSheet.create( {
   inputtext : {
-    borderBottomWidth : 2
-
+    borderBottomWidth : 2 ,
+    padding : 20,
+    marginBottom : 10,
+    fontSize: 24
+  },
+  header: {
+    padding: 40 , 
+    color : "brown" ,
+    fontSize : 24,
+    textAlign : "center",
+    justifyContent : 'center' ,
+    alignItems : 'center'
+  }, 
+  priceborder : {
+    borderWidth : 2 , 
+    marginBottom : 5
   }
 })
