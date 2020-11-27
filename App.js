f700d73f43076017706f55ff97345a545d4e5050
unknown <you@example.com>
2020-11-27 10:04:16 +0500

    init

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+class App extends React.Component{
+  render(){
+    return (
+     <View>
+       <Button title = "new"/>
+       <Text>hello</Text>
+     </View>
+      
+
+    )
+  }
+}
+
+export default App;
